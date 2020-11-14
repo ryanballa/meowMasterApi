c42f480e69df7faa9b38c2bf7ebc99b10ef16b3c
Ryan Balla <ryan@ryanballa.com>
2020-11-14 16:28:31 -0600

    Fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app
                 .select('*')
                 .order('completionDate', { ascending: false })
                 .limit(1);
-            if (moment(lastCleaning[0].completionDate).isBefore(moment().add(2, 'hours'))) {
+            if (moment(lastCleaning[0].completionDate).isAfter(moment().add(2, 'hours'))) {
                 res.status(401).send();
             } else {
                 await supabase
